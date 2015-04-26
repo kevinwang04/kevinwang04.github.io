c087398ed5ba0f3093a17adbafdf6e44a6f9a04b
kevinwang04 <kavinwang04@gmail.com>
2015-04-26 23:09:16 +0800

    learning css animation

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ $(document).ready(function(){
 	var side_h = $(".side_bar").height();
 	$(".side_bar").css("top",(-side_h - 40)+"px");
 	$(".nav_icon").click(function(){
+		
 		$(this).animate({borderRadius:"100%"});
 		if ($(".nav_icon").hasClass("show")) {
 			//隐藏侧边栏
+			$('.side_bar').css({"transform":"translateY((-side_h - 40)px)",
+							"transition-timing-function":"ease-in","-webkit-transition-timing-function":"ease-in"	
+							})
 			$(this).removeClass("show");
-			$(".side_bar").animate({top:(-side_h-40)+"px"});
+			/*$(".side_bar").animate({top:(-side_h-40)+"px"});*/
 			/*$(".page_wrap" ).animate({left:'0px'});
 			$(".side_bar").animate({left:"-250px"});*/
 			$(this).children("span:first").css({"width":"25px","transform":"rotate(0)","position":"relative","top":"0px"});
@@ -17,8 +21,11 @@ $(document).ready(function(){
 			$(this).children("span:odd").css("display","block");
 		}else{
 			//显示侧边栏
+			$('.side_bar').css({"transform":"translateY(0)",
+							"transition-timing-function":"ease-in","-webkit-transition-timing-function":"ease-in"	
+							})
 			$(this).addClass("show");
-			$(".side_bar").animate({top:"0"});
+			/*$(".side_bar").animate({top:"0"});*/
 			/*$(".page_wrap" ).animate({left:'250px'});
 			$(".side_bar").animate({left:"0px"});*/
 			$(this).children("span:first").css({"width":"35px", "transform":"rotate(45deg)","position":"relative","top":"5px"});
@@ -55,7 +62,10 @@ $(document).ready(function(){
 	$(".icon-home").click(function(){
 		$(".ul_wrap").animate({left:"0"},300);
 	});
-
+	$(".strong").css({"cursor":"pointer","position":"relative"});
+	$(".strong").mouseover(function(){
+		$(this).css({"animation":"myfirst 2s ease-in 0 infinite","-webkit-animation":"myfirst 2s ease-in 0 infinite"})
+	});
 });	
 
 //实现个人兴趣介绍栏：
