7071c824d29862a0587abbefed6347bae4b69624
Kevin <kevin@KevindeMacBook-Pro.local>
2015-12-31 00:05:20 +0800

    change index

---
+++ timeline/data.js
@@ -0,0 +1,90 @@
+
+var timelineData = {
+    "timeline":
+    {
+        "headline":"kevin wang<br/> 2014 - 2016",
+        "startDate":"2014,11",
+        "text":"<p>live with nothing but passion</p>",
+        "type":"default",
+        "asset":
+        {
+            "media":"image/dream.gif",
+            "credit":"image: dream",
+            "caption":"Love with nothing but dream。"
+        },
+        "date": [
+            {
+                "startDate":"2014,11,1",
+                "endDate":"2015,1,30",
+                "headline":"first blog",
+                "text":"<p>For someone who never touch web development,it means a lot to me, it's my real hello world of learning FE!</p>",
+                "asset":
+                {
+                    "media":"image/1.png",
+                    "credit":"Apps Capture",
+                    "caption":"It looks a bit ugly ,but I love it!"
+                }
+            },
+            {
+                "startDate":"2015,7",
+                "endDate":"2015,8",
+                "headline":"first discourse website",
+                "text":"using Docker and open-souce discourse to build an online community，just come from an idea of online university societies",
+                "asset":
+                {
+                    "media":"image/2.png",
+                    "credit":"screenshot",
+                    "caption":"that's it!"
+                }
+            },
+            {
+                "startDate":"2015,9",
+                "endDate":"2015,10",
+                "headline":"WiFi manamge system",
+                "text":"First time to contribute code to an company project independently!",
+                "asset":
+                {
+                    "media":"image/3.png",
+                    "credit":"screenshot",
+                    "caption":"Though it just a internal tool, it also should be memory"
+                }
+            },
+            {
+                "startDate":"2015,6",
+                "endDate":"2015,7",
+                "headline":"a epibolic company website",
+                "text":"Cooperate with Backend developer accomplished a epibolic website. It's not complex, just demonstrating something",
+                "asset":
+                {
+                    "media":"image/4.png",
+                    "credit":"screenshot",
+                    "caption":"Got the original feeling of teamwork"
+                }
+            },
+            {
+                "startDate":"2015,11",
+                "endDate":"2015,12",
+                "headline":"a classroom order system for my college",
+                "text":"spend a few day to build this system for my college. Seeing the people around you benefit from you work is a awesome feeling!",
+                "asset":
+                {
+                    "media":"image/5.png",
+                    "credit":"screenshot",
+                    "caption":"now it works!"
+                }
+            },
+            {
+                "startDate":"2015,3",
+                "endDate":"2015,5",
+                "headline":"update blog to 2.0",
+                "text":"Refer to some pretty Responsive web design , redesign the blog ,strengthen it with some plugins",
+                "asset":
+                {
+                    "media":"image/6.png",
+                    "credit":"screenshot",
+                    "caption":"define the UI and UE is a funny process"
+                }
+            }
+        ]
+    }
+}
