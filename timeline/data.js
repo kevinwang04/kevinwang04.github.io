a6eaec6368307926aa7c633a8b5c19863657b40f
Kevin <kevin@KevindeMacBook-Pro.local>
2016-01-01 21:56:16 +0800

    add

--- timeline/data.js
+++ timeline/data.js
@@ -2,8 +2,8 @@
 var timelineData = {
     "timeline":
     {
-        "headline":"kevin wang<br/> 2014 - 2016",
-        "startDate":"2014,11",
+        "headline":"kevin wang<br/> 2015 year",
+        "startDate":"2014,12",
         "text":"<p>live with nothing but passion</p>",
         "type":"default",
         "asset":
@@ -14,7 +14,7 @@ var timelineData = {
         },
         "date": [
             {
-                "startDate":"2014,11,1",
+                "startDate":"2015,1,1",
                 "endDate":"2015,1,30",
                 "headline":"first blog",
                 "text":"<p>For someone who never touch web development,it means a lot to me, it's my real hello world of learning FE!</p>",
@@ -38,7 +38,7 @@ var timelineData = {
                 }
             },
             {
-                "startDate":"2015,9",
+                "startDate":"2015,9,20",
                 "endDate":"2015,10",
                 "headline":"WiFi manamge system",
                 "text":"First time to contribute code to an company project independently!",
@@ -74,7 +74,7 @@ var timelineData = {
                 }
             },
             {
-                "startDate":"2015,3",
+                "startDate":"2015,4",
                 "endDate":"2015,5",
                 "headline":"update blog to 2.0",
                 "text":"Refer to some pretty Responsive web design , redesign the blog ,strengthen it with some plugins",
@@ -83,6 +83,78 @@ var timelineData = {
                     "media":"image/6.png",
                     "credit":"screenshot",
                     "caption":"define the UI and UE is a funny process"
+                }
+            },
+            {
+                "startDate":"2015,5",
+                "endDate":"2015,5",
+                "headline":"a short trip with the sea",
+                "text":"when I walk over the soft sandbeach, it seems like the sea is singing",
+                "asset":
+                {
+                    "media":"image/7.png",
+                    "credit":"photo by wujing",
+                    "caption":"we are so small before the creator!"
+                }
+            },
+            {
+                "startDate":"2015,3",
+                "endDate":"2015,4",
+                "headline":"学院篮球赛",
+                "text":"there may be amount of trifles in life, but when stand in the basketball court, the only thing to think is fighting",
+                "asset":
+                {
+                    "media":"image/8.jpg",
+                    "credit":"photo by zhangtaojun",
+                    "caption":"Basketball === Brother"
+                }
+            },
+            {
+                "startDate":"2015,9",
+                "endDate":"2015,9",
+                "headline":"一生痴绝处，无梦到徽州",
+                "text":"忘不了鳌鱼峰的日落，如同忘不了天都峰和鲫鱼背上的云雾斜阳，忘不了宏村的南湖，如同忘不了属于属于徽州的一水一画",
+                "asset":
+                {
+                    "media":"image/9.png",
+                    "credit":"photo by YT",
+                    "caption":"「试上高峰窥皓月，偶开天眼觊红尘」"
+                }
+            },
+            {
+                "startDate":"2015,8",
+                "endDate":"2015,10",
+                "headline":"First job of life",
+                "text":"有幸加入了一家很酷的公司认识了一群很酷的guy和很负责任的mentor，时常被他们的自由主义意志感染！",
+                "asset":
+                {
+                    "media":"image/10.png",
+                    "credit":"photo from Web",
+                    "caption":"原来业内的正规军是这样干活的呀~"
+                }
+            },
+            {
+                "startDate":"2015,2",
+                "endDate":"2015,5",
+                "headline":"漫长的爬坑",
+                "text":"为了能够清晰了解前端的技术栈，度过了一段艰难而又充实的「技术原始积累」阶段，图书馆的TP-312书架成了老朋友咯",
+                "asset":
+                {
+                    "media":"image/11.jpg",
+                    "credit":"photo by kevin",
+                    "caption":"「大师牛人，宁有种乎？」"
+                }
+            },
+            {
+                "startDate":"2015,12,30",
+                "endDate":"2015,12,30",
+                "headline":"跨年夜！",
+                "text":"和一群臭味相投的伙伴喝得微醺微醺，然后在KTV放肆怒吼，那一刻没有任何声音可以盖过我们",
+                "asset":
+                {
+                    "media":"image/12.png",
+                    "credit":"photo by kevin",
+                    "caption":"「惺惺相惜」原来是件奢饰品"
                 }
             }
         ]
